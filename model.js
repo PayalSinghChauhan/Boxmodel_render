4553baad84146f2f2d46da2b96337d06fdb147e4
Nanu5432 <chauhannaina30@gmail.com>
2020-04-10 21:15:59 +0545

    Boxmodel_render

---
+++ model.js
@@ -0,0 +1,6 @@
+var red = document.createElement("div");
+red.style.width="100px";
+red.style.height="100px";
+red.style.background="red";
+red.style.position="absolute";
+document.body.appendChild(red);
